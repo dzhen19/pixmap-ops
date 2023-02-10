9d0fdd9b0e1f9b30d7274e604c7cc9d2d749ca06
Derrick Zhen <dzhen001@gmail.com>
2023-02-09 22:00:53 -0500

    fixed art

--- src/pixmap_art.cpp
+++ src/pixmap_art.cpp
@@ -10,12 +10,12 @@ int main(int argc, char **argv)
    Image earth;
    earth.load("../images/earth.png");
    Image resized_mark = mark.resize(400, 400);
-   resized_mark.greenscreen(earth);
+   resized_mark.greenscreen(earth).save("mark.png");
 
-   Image scenic;
-   scenic.load("../images/scenic.png");
-   Image result = scenic.gaussian();
-   scenic.save("scenic_gaussian.png");
+   // Image scenic;
+   // scenic.load("../images/scenic.png");
+   // Image result = scenic.gaussian();
+   // scenic.save("scenic_gaussian.png");
 
    return 0;
 }
